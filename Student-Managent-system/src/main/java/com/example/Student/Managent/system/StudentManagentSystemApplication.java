aa392903db8c66f4fd8ebb5404eab6ab8eec6ffc
Pham Van Quyen <quyenvanpham636@gmail.com>
2025-07-29 21:38:58 +0700

    [Start] Start build a Spring framwork for project

---
+++ Student-Managent-system/src/main/java/com/example/Student/Managent/system/StudentManagentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student.Managent.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagentSystemApplication.class, args);
+	}
+
+}
---
+++ Student-Managent-system/src/test/java/com/example/Student/Managent/system/StudentManagentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Student.Managent.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
